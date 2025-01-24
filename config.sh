563f8f2322e76c0cfb65f19f063cb5696de5be16
l1f21bsse0227 <l1f21bsse0227@ucp.edu.pk>
2025-01-25 00:34:16 +0500

    Create folder structure & Add package.json

---
+++ config.sh
@@ -0,0 +1,7 @@
+echo "Going to configure a repository"
+git branch -m develop 
+git config --local user.name "l1f21bsse0227"
+git config --local user.email "l1f21bsse0227@ucp.edu.pk"
+git config --local core.editor notepad
+mkdir ./src ./styles
+touch ./src/index.html ./styles/index.css .gitignore
